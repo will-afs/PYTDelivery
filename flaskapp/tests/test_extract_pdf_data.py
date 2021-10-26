c4cd489dd9113a17448f5dbc3b85d0e311831afc
William AFONSO <william.afonso.pro@gmail.com>
2021-10-26 16:05:04 +0200

    Created unit tests for extract PFD content core function

--- flaskapp/tests/test_extract_pdf_data.py
+++ flaskapp/tests/test_extract_pdf_data.py
@@ -1,14 +1,15 @@
 import unittest
 from pdfminer.pdfparser import PDFSyntaxError
-from flaskapp.extract_pdf_data import extract_pdf_metadata
+from flaskapp.core.extract_pdf_data import extract_pdf_metadata, extract_pdf_content
 
 # tests settings
-DATA_FILE_PATH = './flaskapp/resources/'
-PDF_DATA_FILE_NAME = 'DOSSIER_COUP_DE_POUCE_2021.pdf'
+DATA_FILE_PATH = './flaskapp/tests/resources/'
+PDF_DATA_FILE_NAME = 'PDF_FILE.pdf'
 NOT_EXISTING_DATA_FILE_NAME = 'file_name_which_does_not_exist'
 NOT_PDF_DATA_FILE_NAME = 'dummy.text'
+PDF_CONTENT_REFERENCE = 'pdf_content_reference.txt'
 
-class TestExtractPDFData(unittest.TestCase):
+class TestExtractPDFMetadata(unittest.TestCase):
 
     def test_extract_pdf_metadata_file_not_found(self):
         with self.assertRaises(FileNotFoundError):
@@ -29,3 +30,20 @@ class TestExtractPDFData(unittest.TestCase):
                             }
         extracted_metadata = extract_pdf_metadata(DATA_FILE_PATH + PDF_DATA_FILE_NAME)
         self.assertEqual(extracted_metadata, reference_metadata)
+
+
+class TestExtractPDFContent(unittest.TestCase):
+
+    def test_extract_pdf_content_file_not_found(self):
+        with self.assertRaises(FileNotFoundError):
+            extract_pdf_content(DATA_FILE_PATH + NOT_EXISTING_DATA_FILE_NAME)
+
+    def test_extract_pdf_content_file_not_pdf(self):
+        with self.assertRaises(PDFSyntaxError):
+            extract_pdf_content(DATA_FILE_PATH + NOT_PDF_DATA_FILE_NAME)
+
+    def test_extract_pdf_content_file_success(self):
+        with open(DATA_FILE_PATH + PDF_CONTENT_REFERENCE, 'r') as pdf_content_reference_file:
+            reference_content = pdf_content_reference_file.read()
+        extracted_content = extract_pdf_content(DATA_FILE_PATH + PDF_DATA_FILE_NAME)
+        self.assertEqual(extracted_content, reference_content)
