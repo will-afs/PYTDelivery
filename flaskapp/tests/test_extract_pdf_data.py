49d34764231cf99d3822d0e71a190ae878e947d6
William AFONSO <william.afonso.pro@gmail.com>
2021-12-03 08:14:54 +0100

    Solves #6 : created a test configuration file ("test_extract_pdf_data.cfg")

--- flaskapp/tests/test_extract_pdf_data.py
+++ flaskapp/tests/test_extract_pdf_data.py
@@ -1,13 +1,19 @@
 import unittest
 from pdfminer.pdfparser import PDFSyntaxError
 from flaskapp.core.extract_pdf_data import extract_pdf_metadata, extract_pdf_content
+import configparser, json
 
-# tests settings
-DATA_FILE_PATH = './flaskapp/tests/resources/'
-PDF_DATA_FILE_NAME = 'PDF_FILE.pdf'
-NOT_EXISTING_DATA_FILE_NAME = 'file_name_which_does_not_exist'
-NOT_PDF_DATA_FILE_NAME = 'dummy.text'
-PDF_CONTENT_REFERENCE = 'pdf_content_reference.txt'
+
+config = configparser.ConfigParser()
+config.read('./flaskapp/tests/test_extract_pdf_data.cfg')
+
+DATA_FILE_PATH = config['PATHS']['DATA_FILE_PATH']
+NOT_EXISTING_DATA_FILE_NAME = config['PATHS']['NOT_EXISTING_DATA_FILE_NAME']
+NOT_PDF_DATA_FILE_NAME = config['PATHS']['NOT_PDF_DATA_FILE_NAME']
+PDF_DATA_FILE_NAME = config['PATHS']['PDF_DATA_FILE_NAME']
+PDF_CONTENT_REFERENCE = config['PATHS']['PDF_CONTENT_REFERENCE']
+PDF_METADATA_REFERENCE = config['PATHS']['PDF_METADATA_REFERENCE']
+
 
 class TestExtractPDFMetadata(unittest.TestCase):
 
@@ -20,14 +26,8 @@ class TestExtractPDFMetadata(unittest.TestCase):
             extract_pdf_metadata(DATA_FILE_PATH + NOT_PDF_DATA_FILE_NAME)
 
     def test_extract_pdf_metadata_file_success(self):
-        reference_metadata = {
-                                'Author': 'AURORE',
-                                'CreationDate': "D:20200325185329+01'00'",
-                                'Creator': 'Microsoft Office Word 2007',
-                                'ModDate': "D:20210311153835+01'00'",
-                                'Producer': 'Microsoft Office Word 2007',
-                                'Title': 'DOSSIER COUP DE POUCE 2020'
-                            }
+        with open(DATA_FILE_PATH + PDF_METADATA_REFERENCE, 'r') as pdf_metadata_reference_file:
+            reference_metadata = json.load(pdf_metadata_reference_file)
         extracted_metadata = extract_pdf_metadata(DATA_FILE_PATH + PDF_DATA_FILE_NAME)
         self.assertEqual(extracted_metadata, reference_metadata)
 
