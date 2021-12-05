c1a9df06c243e18ad8cc68a8e56e4108d9de3040
William AFONSO <william.afonso.pro@gmail.com>
2021-12-05 13:18:41 +0100

    - From now on, PDF can only be downloaded from a URI, not uploaded from a local file - New function "extract_pdf_data" to extract both content and metadata from a remote PDF file accessible through a URI - From now on, PDF are stored temporarily in a tmp directory, and deleted as soon as they are not useful anymore

--- flaskapp/core/extract_pdf_data.py
+++ flaskapp/core/extract_pdf_data.py
@@ -1,8 +1,66 @@
+from typing import List, Literal
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.high_level import extract_text
+import urllib, os, glob
+from pdfminer.pdfparser import PDFSyntaxError
 
+TMP_DIRECTORY = './tmp'
 
+def is_pdf(file_local_path:str)->bool:
+    """Assuming the file being pointed by the URI exists, checks whether it is a PDF
+
+    Parameters:
+    file_local_path (str) : the path to access the local file
+
+    Returns:
+    bool: Whether the file is a PDF (True) or not (False)
+    """
+    is_pdf = True
+    with open(file_local_path, 'rb') as pdf_file:
+        try:
+            PDFDocument(PDFParser(pdf_file)) # Should raise a PDFSyntaxError exception if not a real PDF
+        except PDFSyntaxError:
+            is_pdf = False
+    return is_pdf
+
+def clear_dir(dir_path:str=TMP_DIRECTORY):
+    files = glob.glob(dir_path + '/*')
+    for f in files:
+        os.remove(f)
+
+def download_pdf(file_uri:str, directory:str='./tmp') -> str:
+    """Downloads a PDF acessible from a URI and stores it into the specified directory
+
+    Parameters:
+    file_uri (str) : the URI to access the file
+    directory (str) : the directory into which saving the file
+
+    Returns:
+    str: The path to access the PDF saved locally
+    """
+    try:
+        web_file = urllib.request.urlopen(file_uri)
+    except urllib.error.HTTPError:
+        raise FileNotFoundError('Could not access the provided URI')
+    except ValueError:
+        raise ValueError('Wrong URI format')
+    else:
+        file_name = file_uri.split('/')[-1]
+        pdf_name = file_name.replace('.pdf', '') + '.pdf' # Delete eventual ".pdf" before adding it
+        pdf_local_path = directory + '/' + pdf_name
+        if not os.path.isfile(pdf_local_path):
+            with open(pdf_local_path, 'wb') as pdf_file:
+                pdf_file.write(web_file.read())
+            web_file.close()
+            if is_pdf(pdf_local_path):
+                return pdf_local_path
+            else:
+                os.remove(pdf_local_path)
+                raise PDFSyntaxError('The file being pointed by the provided URI does not seem to be a PDF')
+        else:
+            raise FileExistsError('File \"{}\" already exists'.format(pdf_name))   
+        
 def extract_pdf_metadata(pdf_path:str) -> dict:
     """Returns the metadata of a PDF
 
@@ -10,7 +68,18 @@ def extract_pdf_metadata(pdf_path:str) -> dict:
     pdf_path (str) : the path of the PDF file of which metadata should be extracted
 
     Returns:
-    dict: metadata of the PDF
+    dict: metadata of the PDF, presented as a JSON structured as follows : 
+        {
+            metadata:{
+                        "Author": "AURORE",
+                        "CreationDate": "D:20200325185329+01'00'",
+                        "Creator": "Microsoft Office Word 2007",
+                        "ModDate": "D:20210311153835+01'00'",
+                        "Producer": "Microsoft Office Word 2007",
+                        "Title": "DOSSIER COUP DE POUCE 2020"
+                    }
+            content:"Lorem ipsum dolor sit amet, ..."
+        }
     """
     with open(pdf_path, 'rb') as pdf_file:
         pdf_parser = PDFParser(pdf_file)
@@ -33,3 +102,26 @@ def extract_pdf_content(pdf_path:str) -> str:
     with open(pdf_path,'rb') as pdf_file:
         content_as_string = extract_text(pdf_file)
         return content_as_string
+
+def extract_pdf_data(pdf_uri:str) -> List:
+    """Extracts PDF data acessible from a URI
+
+    Parameters:
+    pdf_uri (str) : the URI through which access the file
+
+    Returns:
+    dict: metadata of the PDF, presented as a JSON structured as follows : 
+        {
+            "Producer": "GPL Ghostscript SVN PRE-RELEASE 8.62", 
+            "CreationDate": "D:20080203020500-05'00'", 
+            "ModDate": "D:20080203020500-05'00'", 
+            "Creator": "dvips 5.499 Copyright 1986, 1993 Radical Eye Software",
+            "Title": "dynamic.dvi"
+        }
+    str: PDF content
+    """
+    pdf_path = download_pdf(pdf_uri, TMP_DIRECTORY)
+    pdf_metadata = extract_pdf_metadata(pdf_path)
+    pdf_content = extract_pdf_content(pdf_path)
+    clear_dir(TMP_DIRECTORY) # Remove the temporarily created file
+    return pdf_metadata, pdf_content
--- tests/test_extract_pdf_data.py
+++ tests/test_extract_pdf_data.py
@@ -1,11 +1,12 @@
 import unittest
 from pdfminer.pdfparser import PDFSyntaxError
-from flaskapp.core.extract_pdf_data import extract_pdf_metadata, extract_pdf_content
-import configparser, json
+from flaskapp.core.extract_pdf_data import clear_dir, extract_pdf_metadata, extract_pdf_content, download_pdf, is_pdf, extract_pdf_data
+import configparser, json, os, glob
 
 
 config = configparser.ConfigParser()
-config.read('./tests/test_extract_pdf_data.cfg')
+TESTS_DIRECTORY = './tests'
+config.read(TESTS_DIRECTORY + '/test_extract_pdf_data.cfg')
 
 DATA_FILE_PATH = config['PATHS']['DATA_FILE_PATH']
 NOT_EXISTING_DATA_FILE_NAME = config['PATHS']['NOT_EXISTING_DATA_FILE_NAME']
@@ -13,6 +14,51 @@ NOT_PDF_DATA_FILE_NAME = config['PATHS']['NOT_PDF_DATA_FILE_NAME']
 PDF_DATA_FILE_NAME = config['PATHS']['PDF_DATA_FILE_NAME']
 PDF_CONTENT_REFERENCE = config['PATHS']['PDF_CONTENT_REFERENCE']
 PDF_METADATA_REFERENCE = config['PATHS']['PDF_METADATA_REFERENCE']
+WRONG_PDF_URI = config['PATHS']['WRONG_PDF_URI']
+NOT_FOUND_PDF_URI = config['PATHS']['NOT_FOUND_PDF_URI']
+NOT_PDF_URI = config['PATHS']['NOT_PDF_URI']
+PDF_URI = config['PATHS']['PDF_URI']
+TMP_DIRECTORY = config['PATHS']['TMP_DIRECTORY']
+
+class TestIsPDF(unittest.TestCase):
+
+    def test_is_pdf_success(self):
+        self.assertTrue(is_pdf(DATA_FILE_PATH+PDF_DATA_FILE_NAME))
+
+    def test_is_pdf_fails(self):
+        self.assertFalse(is_pdf(DATA_FILE_PATH+NOT_PDF_DATA_FILE_NAME))
+    
+
+class TestDownloadPDF(unittest.TestCase):
+    def setUp(self) -> None:
+        # Ensure there is no file in the folder before running these tests
+        clear_dir(TMP_DIRECTORY)
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        # Ensure there is no file in the folder after running these tests
+        clear_dir(TMP_DIRECTORY)
+        return super().tearDown()
+        
+    def test_download_pdf_wrong_uri_format(self):
+        with self.assertRaises(ValueError):
+            download_pdf(WRONG_PDF_URI)
+
+    def test_download_pdf_file_not_found(self):
+        with self.assertRaises(FileNotFoundError):
+            download_pdf(NOT_FOUND_PDF_URI)
+    
+    def test_download_pdf_file_not_pdf(self):
+        with self.assertRaises(PDFSyntaxError):
+            download_pdf(NOT_PDF_URI)
+
+    def test_download_pdf_file_already_exists(self):
+        download_pdf(PDF_URI)
+        with self.assertRaises(FileExistsError):
+            download_pdf(PDF_URI)
+
+    def test_download_pdf_success(self):
+        self.assertTrue(os.path.isfile(download_pdf(PDF_URI)))
 
 
 class TestExtractPDFMetadata(unittest.TestCase):
@@ -47,3 +93,30 @@ class TestExtractPDFContent(unittest.TestCase):
             reference_content = pdf_content_reference_file.read()
         extracted_content = extract_pdf_content(DATA_FILE_PATH + PDF_DATA_FILE_NAME)
         self.assertEqual(extracted_content, reference_content)
+
+
+class TestExtractPDFData(unittest.TestCase):
+
+    def setUp(self) -> None:
+        # Ensure there is no file in the folder before running these tests
+        clear_dir(TMP_DIRECTORY)
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        # Ensure there is no file in the folder after running these tests
+        clear_dir(TMP_DIRECTORY)
+        return super().tearDown()
+
+    def test_extract_pdf_data_success(self):
+        extracted_metadata, extracted_content = extract_pdf_data(PDF_URI)
+        # Checks metadata
+        with open(DATA_FILE_PATH + PDF_METADATA_REFERENCE, 'r') as pdf_metadata_reference_file:
+            reference_metadata = json.load(pdf_metadata_reference_file)
+        self.assertEqual(extracted_metadata, reference_metadata)
+        # Checks content
+        with open(DATA_FILE_PATH + PDF_CONTENT_REFERENCE, 'r') as pdf_content_reference_file:
+            reference_content = pdf_content_reference_file.read()
+        self.assertEqual(extracted_content, reference_content)
+        # Checks whether temporary PDF file has been successfuly deleted from tmp directory
+        files = glob.glob(TMP_DIRECTORY + '/*')
+        self.assertEqual(files, [])
